49140eeed54f617e9bfd76a8625c19f9cf5d30bf
nirav patel <nirav.ce.2008@gmail.com>
2014-06-14 17:31:43 +0530

    Location related changes

--- public/js/admin/dealer/index.js
+++ public/js/admin/dealer/index.js
@@ -15,17 +15,26 @@ $(document).ready(function() {
 		]
 	} );
 
+	var tmp_address = $('#de_address').val();
+	if ($('#de_city').val().trim() != "")
+		tmp_address += ", "+$('#de_city').val()
+	if ($('#de_state').val().trim() != "")
+		tmp_address += ", "+$('#de_state').val()
+	if ($('#de_zip').val().trim() != "")
+		tmp_address += ", "+$('#de_zip').val()
+
+	$('#de_address_tmp').val(tmp_address);
+	
 	$('#locationHolder').locationpicker({
 		radius: 300,
 		inputBinding: {
-			locationNameInput: $('#de_address'),
-			latitudeInput: $('#de_lat'),
-			longitudeInput: $('#de_long'),
+			locationNameInput: $('#de_address_tmp'),
 		},
 		enableAutocomplete: true,
 		onchanged: function(currentLocation, radius, isMarkerDropped) {
-				console.log(currentLocation);
-			//alert("Location changed. New location (" + currentLocation.latitude + ", " + currentLocation.longitude + ")");
+				$('#de_lat').val(currentLocation.latitude),
+				$('#de_long').val(currentLocation.longitude)
+			//alert("Location changed. New location (" +  + ", " +  + ")");
 		}
 	});
 
