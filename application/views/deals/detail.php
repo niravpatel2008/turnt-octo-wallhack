853710f42c4b5648d47cde8e0bcfc8825c42860d
nirav patel <nirav.ce.2008@gmail.com>
2014-07-03 16:29:13 +0530

    detail html

--- application/views/deals/detail.php
+++ application/views/deals/detail.php
@@ -3,5 +3,54 @@
 //print_r($dealsDetail);
 ?>
 <div class='stripe-regular items-carousel-wrap row'>
-	This is testing	
+	<div class="box box-success">
+		<div class="box-header">
+			<h3 class="box-title"><i class="fa fa-comments-o"></i> Title</h3>
+		</div>
+		<div class="box-body">
+			<div id='buy'>
+			</div>
+			<div id='slider'>
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+						<li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="http://placehold.it/900x500/39CCCC/ffffff&text=I+Love+Bootstrap" alt="First slide">
+							<div class="carousel-caption">
+								First Slide
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://placehold.it/900x500/3c8dbc/ffffff&text=I+Love+Bootstrap" alt="Second slide">
+							<div class="carousel-caption">
+								Second Slide
+							</div>
+						</div>
+						<div class="item">
+							<img src="http://placehold.it/900x500/f39c12/ffffff&text=I+Love+Bootstrap" alt="Third slide">
+							<div class="carousel-caption">
+								Third Slide
+							</div>
+						</div>
+					</div>
+					<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left"></span>
+					</a>
+					<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+					</a>
+				</div>
+			</div>
+			<div id='detail'>
+			</div>
+			<div id='map'>
+			</div>
+		</div>		
+		<div class="box-footer">
+		</div>
+	<div>
 </div>
