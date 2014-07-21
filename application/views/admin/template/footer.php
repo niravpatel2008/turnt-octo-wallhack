06f22b6cf71989782b7cc7ad208cefa48aa72e9b
aniketharshe20 <aniketharshe20@gmail.com>
2014-07-21 21:27:19 +0530

    admin footer.php

--- application/views/admin/template/footer.php
+++ application/views/admin/template/footer.php
@@ -25,7 +25,7 @@
     <!-- iCheck -->
     <script src="<?=public_path()?>js/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
 
-    
+
     <script type="text/javascript">
         function admin_path () {
             return '<?=admin_path()?>';
@@ -47,14 +47,18 @@
                             <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button> \n\
                             '+msg+' \n\
                         </div>';
-            return html;        
+            return html;
         }
     </script>
+
+    <?php if ($this->router->fetch_class() == "dashboard") { ?>
+        <script src="<?=public_path()?>js/admin/dashboard/deals.js" type="text/javascript"></script>
+    <?php } ?>
 
     <?php if ($this->router->fetch_class() == "users") { ?>
 		<script src="<?=public_path()?>js/admin/users/index.js" type="text/javascript"></script>
     <?php } ?>
-	
+
     <?php if ($this->router->fetch_class() == "category") { ?>
 		<script src="<?=public_path()?>js/admin/category/index.js" type="text/javascript"></script>
     <?php } ?>
@@ -72,10 +76,10 @@
 
     <!-- AdminLTE App -->
     <script src="<?=public_path()?>js/AdminLTE/app.js" type="text/javascript"></script>
-    
+
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="<?=public_path()?>js/AdminLTE/dashboard.js" type="text/javascript"></script>     
-    
+    <script src="<?=public_path()?>js/AdminLTE/dashboard.js" type="text/javascript"></script>
+
     <!-- AdminLTE for demo purposes -->
     <script src="<?=public_path()?>js/AdminLTE/demo.js" type="text/javascript"></script>
 
