8ad73e1b18a542cd3c4321bb10319e935b086b6c
Nirav <nirav.ce.2008@gmail.com>
2014-07-31 23:43:37 -0700

    Email change

--- application/controllers/admin/index.php
+++ application/controllers/admin/index.php
@@ -94,7 +94,7 @@ class Index extends CI_Controller {
 
 					$newpassword = random_string('alnum', 8);
 					$data = array('du_password' => sha1($newpassword));
-					$user = $this->common_model->updateData(DEAL_USER,$data,$where);
+					$upid = $this->common_model->updateData(DEAL_USER,$data,$where);
 
 					$login_details = array('username' => $user[0]->du_uname,'password' => $newpassword);
 					$emailTpl = $this->get_forgotpassword_tpl($login_details);
